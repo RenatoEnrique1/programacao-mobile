5c012092080281966c99602430c4caa77f5725c0
RenatoEnrique1 <81253831+RenatoEnrique1@users.noreply.github.com>
2023-03-24 22:00:31 -0300

    Delete ex2.kt

--- estudodirigido2/ex2.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    for (num in 0..10) {
-        var fatorial = 1
-        for (i in 1..num) {
-            fatorial *= i
-        }
-        println("O fatorial de $num é $fatorial")
-    }
-}
